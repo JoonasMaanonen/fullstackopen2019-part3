1f1524abdfe8cfbcc883add94a5ea334918017f2
Joonas Maanonen <joonas.maanonen@aalto.fi>
2019-11-19 10:28:44 +0200

    Add frontend prod build

---
+++ build/precache-manifest.f61ce17ada26baa404e2055291b9ce46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab0a0d8513c8e1bb5bf7a6753a49cd4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cfc3b95bd8e47fa661a",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "897745daf7f8a8f969be",
+    "url": "/static/js/2.bbdc062d.chunk.js"
+  },
+  {
+    "revision": "1cfc3b95bd8e47fa661a",
+    "url": "/static/js/main.6985fe35.chunk.js"
+  },
+  {
+    "revision": "3da10799abf0dbd55beb",
+    "url": "/static/js/runtime-main.b1e82656.js"
+  }
+]);
